d1989ea086e8f4448be83908bc324578d3c024c7
pankowskitomasz <pankowski.email@gmail.com>
2025-03-30 11:54:26 +0200

    routing,layout: signin,signup,restorepassword,passwordupdate

---
+++ src/views/job/jobDetailsView.js
@@ -0,0 +1,12 @@
+import {Container } from "react-bootstrap";
+
+function JobDetailsView(){
+    return(
+        <Container>
+            <h1>Job Details View</h1>
+
+        </Container>
+    );
+}
+
+export default JobDetailsView;
---
+++ src/views/job/jobListView.js
@@ -0,0 +1,12 @@
+import {Container } from "react-bootstrap";
+
+function JobListView(){
+    return(
+        <Container>
+            <h1>Job List View</h1>
+
+        </Container>
+    );
+}
+
+export default JobListView;
